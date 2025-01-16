2aa096b334d595c8b4ef9911e5ce8fb4e24a19bb
Eliot LEROUX <elleroux@student.42lyon.fr>
2025-01-16 18:46:48 +0100

    asset opening for wall now outside of the bonus if

--- src/cub.c
+++ src/cub.c
@@ -89,41 +89,6 @@ int	main(int argc, char **argv)
 		player_dot.data = mlx_get_data_addr(player_dot.img_ptr, &(player_dot.bpp), &(player_dot.line_len), &(player_dot.endian));
 		data.player_dot = &player_dot;
 
-		t_asset text_n;
-		ft_bzero(&text_n, sizeof(t_asset));
-		text_n.path = "assets/img/text_n/text_n.xpm";
-		text_n.img_ptr = mlx_xpm_file_to_image(data.mlx, text_n.path, &(text_n.width), &(text_n.height));
-		if (text_n.img_ptr == NULL)
-			printf("Failed to open %s.\n", text_n.path);
-		text_n.data = mlx_get_data_addr(text_n.img_ptr, &(text_n.bpp), &(text_n.line_len), &(text_n.endian));
-		data.text_n = &text_n;
-
-		t_asset text_s;
-		ft_bzero(&text_s, sizeof(t_asset));
-		text_s.path = "assets/img/text_s/text_s.xpm";
-		text_s.img_ptr = mlx_xpm_file_to_image(data.mlx, text_s.path, &(text_s.width), &(text_s.height));
-		if (text_s.img_ptr == NULL)
-			printf("Failed to open %s.\n", text_s.path);
-		text_s.data = mlx_get_data_addr(text_s.img_ptr, &(text_s.bpp), &(text_s.line_len), &(text_s.endian));
-		data.text_s = &text_s;
-
-		t_asset text_e;
-		ft_bzero(&text_e, sizeof(t_asset));
-		text_e.path = "assets/img/text_e/text_e.xpm";
-		text_e.img_ptr = mlx_xpm_file_to_image(data.mlx, text_e.path, &(text_e.width), &(text_e.height));
-		if (text_e.img_ptr == NULL)
-			printf("Failed to open %s.\n", text_e.path);
-		text_e.data = mlx_get_data_addr(text_e.img_ptr, &(text_e.bpp), &(text_e.line_len), &(text_e.endian));
-		data.text_e = &text_e;
-
-		t_asset text_w;
-		ft_bzero(&text_w, sizeof(t_asset));
-		text_w.path = "assets/img/text_w/text_w.xpm";
-		text_w.img_ptr = mlx_xpm_file_to_image(data.mlx, text_w.path, &(text_w.width), &(text_w.height));
-		if (text_w.img_ptr == NULL)
-			printf("Failed to open %s.\n", text_w.path);
-		text_w.data = mlx_get_data_addr(text_w.img_ptr, &(text_w.bpp), &(text_w.line_len), &(text_w.endian));
-		data.text_w = &text_w;
 
 		t_list* arm_running;
 		arm_running = init_asset(data, "arm_running_fast/", 21);
@@ -137,6 +102,41 @@ int	main(int argc, char **argv)
 		arm_finger = init_asset(data, "arm_finger/", 17);
 		data.arm_finger = arm_finger;
 	}
+	t_asset text_n;
+	ft_bzero(&text_n, sizeof(t_asset));
+	text_n.path = "assets/img/text_n/text_n.xpm";
+	text_n.img_ptr = mlx_xpm_file_to_image(data.mlx, text_n.path, &(text_n.width), &(text_n.height));
+	if (text_n.img_ptr == NULL)
+		printf("Failed to open %s.\n", text_n.path);
+	text_n.data = mlx_get_data_addr(text_n.img_ptr, &(text_n.bpp), &(text_n.line_len), &(text_n.endian));
+	data.text_n = &text_n;
+
+	t_asset text_s;
+	ft_bzero(&text_s, sizeof(t_asset));
+	text_s.path = "assets/img/text_s/text_s.xpm";
+	text_s.img_ptr = mlx_xpm_file_to_image(data.mlx, text_s.path, &(text_s.width), &(text_s.height));
+	if (text_s.img_ptr == NULL)
+		printf("Failed to open %s.\n", text_s.path);
+	text_s.data = mlx_get_data_addr(text_s.img_ptr, &(text_s.bpp), &(text_s.line_len), &(text_s.endian));
+	data.text_s = &text_s;
+
+	t_asset text_e;
+	ft_bzero(&text_e, sizeof(t_asset));
+	text_e.path = "assets/img/text_e/text_e.xpm";
+	text_e.img_ptr = mlx_xpm_file_to_image(data.mlx, text_e.path, &(text_e.width), &(text_e.height));
+	if (text_e.img_ptr == NULL)
+		printf("Failed to open %s.\n", text_e.path);
+	text_e.data = mlx_get_data_addr(text_e.img_ptr, &(text_e.bpp), &(text_e.line_len), &(text_e.endian));
+	data.text_e = &text_e;
+
+	t_asset text_w;
+	ft_bzero(&text_w, sizeof(t_asset));
+	text_w.path = "assets/img/text_w/text_w.xpm";
+	text_w.img_ptr = mlx_xpm_file_to_image(data.mlx, text_w.path, &(text_w.width), &(text_w.height));
+	if (text_w.img_ptr == NULL)
+		printf("Failed to open %s.\n", text_w.path);
+	text_w.data = mlx_get_data_addr(text_w.img_ptr, &(text_w.bpp), &(text_w.line_len), &(text_w.endian));
+	data.text_w = &text_w;
     mlx_hook(data.win, DESTROY_NOTIFY, 0, close_window, &data);
     mlx_loop_hook(data.mlx, handle_movement, &data);
 
